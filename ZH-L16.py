6a6d3dd0a43bbeebbabd3ce56ce7da0b8b107998
jeffreyquicken <jeffrey.quicken@student.kuleuven.be>
2020-06-09 20:22:53 +0200

    Add basic functions for pressure calculations

--- ZH-L16.py
+++ ZH-L16.py
@@ -1,6 +1,10 @@
 import math
 
-""" Half-time values for Nitrogen with associated 'b' value and 'a' value for ZH-L16A, ZH-L16B and ZH-L16C"""
+# Half-time values for Nitrogen with associated 'b' value and 'a' value for ZH-L16A, ZH-L16B and ZH-L16C
+# ZH-L16A: original without conservatism
+# ZH-L16B: more conservative 'a' values (for dive table construction)
+# ZH-L16C: faster 'a' values
+
 ZHL16N = {
     1   : { "t" : 4.0,   "b" : 0.505,  "a" : { "A" : 1.2599, "B" : 1.2599, "C" : 1.2599 } },
     1.1 : { "t" : 5.0,   "b" : 0.5578, "a" : { "A" : 1.1696, "B" : 1.1696, "C" : 1.1696 } },
@@ -20,3 +24,41 @@ ZHL16N = {
     15  : { "t" : 498.0, "b" : 0.9602, "a" : { "A" : 0.2523, "B" : 0.2523, "C" : 0.248 } },
     16  : { "t" : 635.0, "b" : 0.9653, "a" : { "A" : 0.2327, "B" : 0.2327, "C" : 0.2327 } }
 }
+
+
+def depth_to_atm(depth):
+    """
+    Converts dive depth (m) to hydrostatic pressure (atm)
+
+    :param depth: dive depth in meters
+    :return: pressure in atm (1 atmosphere = 1 bar)
+    """
+    return depth/10 + 1
+
+
+def calculate_inert_gas_pressure_compartment(p_begin, pp_gasmix, t_exposure, halftime, depth):
+    """
+    Calculates pressure of inert gas in compartment
+
+    WARNING -- This formula assumes the descent is instant. Averaging is necessary for more realistic values
+    :param p_begin: Inert gas pressure in the compartment before the exposure (atm)
+    :param pp_gasmix: Partial pressure of the inert gas breathed by the diver
+    :param t_exposure:
+    :param halftime:
+    :return:
+    """
+    p_depth = depth_to_atm(depth)
+    p_gasmix = pp_gasmix * p_depth  # P of inert gas at depth is partial P of inert gas multiplied by total P at depth
+    return p_begin + (p_gasmix - p_begin) * (1 - 2 ** (- t_exposure / halftime))
+
+def calculate_tolerated_ambient_pressure_compartment(a, b, p_comp):
+    """
+    Calculates the maximum tolerated ambient pressure we can safely ascend to for a given compartment (a and b)
+    without bubble formation in compartment
+    :param a: a modifier as given by the ZH-L16 variant
+    :param b: b modifier as given by the ZH-L16 variant
+    :param p_comp: Inert gas pressure in the compartment
+    :return:
+    """
+    return ((p_comp - a) * b)
+
